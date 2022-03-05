a4f74f80e7849253b52c0df84385abdd39744929
Mustafa Nofal <mnofal225@gmail.com>
2022-03-04 22:34:40 -0600

    formatted code to be cleaner and fixed balanceTotal and expenseTotal bug after delete

--- budget.js
+++ budget.js
@@ -1,151 +1,104 @@
-const budgetAmount =  document.querySelector('#budgetAmount');
-const budgetTotal = document.querySelector('#budgettotal');
-const budgetCalculateBtn = document.querySelector('#budgetCalculateBtn');
-const balanceTotal =  document.querySelector('#balancetotal');
-const expenseName = document.querySelector('#expensename');
-const expenseAmount = document.querySelector('#expenseamount');
-const addExpenseBtn = document.querySelector('#addexpensebtn');
-const expenseTotal = document.querySelector('#expensetotal');
-const expenseTitle = document.querySelector('#expensetitle');
-const expenseValue = document.querySelector('#expensevalue');
-const editAndDelete = document.querySelector('#editanddelete');
+const budgetAmount = document.querySelector("#budgetAmount");
+const budgetTotal = document.querySelector("#budgettotal");
+const budgetCalculateBtn = document.querySelector("#budgetCalculateBtn");
+const balanceTotal = document.querySelector("#balancetotal");
+const expenseName = document.querySelector("#expensename");
+const expenseAmount = document.querySelector("#expenseamount");
+const addExpenseBtn = document.querySelector("#addexpensebtn");
+const expenseTotal = document.querySelector("#expensetotal");
+const expenseTitle = document.querySelector("#expensetitle");
+const expenseValue = document.querySelector("#expensevalue");
+const editAndDelete = document.querySelector("#editanddelete");
 let expenseList = [];
-let testlist = [];
 
-
-const getBudgetAmount = budgetCalculateBtn.addEventListener('click', ()=> {
-    event.preventDefault();
-    budgetTotal.innerText = budgetAmount.value;
-    balanceTotal.innerText = parseFloat(budgetAmount.value) - parseFloat(expenseTotal.innerText);
-}) 
+const getBudgetAmount = budgetCalculateBtn.addEventListener("click", () => {
+  event.preventDefault();
+  budgetTotal.innerText = budgetAmount.value;
+  calculateBalanceTotal();
+  
+});
 
 const populateList = (arr) => {
-    let itemName = document.createElement('li') ;
-    let itemValue = document.createElement('li');
-    for(i = 0; i < arr.length; i++){
-        itemName.innerText = arr[i].name
-        itemValue.innerText = arr[i].value
-        expenseTitle.appendChild(itemName);
-        expenseValue.appendChild(itemValue);
+  let itemName = document.createElement("li");
+  let itemValue = document.createElement("li");
+  for (i = 0; i < arr.length; i++) {
+    itemName.innerText = arr[i].name;
+    itemValue.innerText = arr[i].value;
+    expenseTitle.appendChild(itemName);
+    expenseValue.appendChild(itemValue);
+  }
+};
+
+const addExpensesList = addExpenseBtn.addEventListener("click", () => {
+  event.preventDefault();
+  let counter = 1 + expenseList.length;
+  let expenseDetails = {
+    name: expenseName.value,
+    value: parseFloat(expenseAmount.value),
+    id: counter,
+  };
+  expenseList.push(expenseDetails);
+
+  populateList(expenseList);
+
+  let options = document.createElement("li");
+  let appendOptions = editAndDelete.appendChild(options);
+
+  let deleteItem = document.createElement("a");
+  deleteItem.href = "#";
+  deleteItem.innerText = "delete";
+  deleteItem.addEventListener("click", () => {
+    deleteItemFunc(expenseList);
+  });
+  appendOptions.appendChild(deleteItem);
+
+  let deleteItemFunc = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id === expenseDetails.id) {
+        arr.splice(i, 1);
+        expenseTitle.removeChild(expenseTitle.children[i]);
+        expenseValue.removeChild(expenseValue.children[i]);
+        editAndDelete.removeChild(editAndDelete.children[i]);
+        calculateExpenseTotal();
+        calculateBalanceTotal();
+        
+      }
     }
-    
-    console.log('m',arr);
-}
+  };
+  calculateExpenseTotal();
+  calculateBalanceTotal();
+  
+});
 
-const addExpensesList = addExpenseBtn.addEventListener('click', () => {
-    event.preventDefault();
-    let counter = 1 + expenseList.length;
-    let  expenseDetails = {
-        name : expenseName.value, 
-        value: parseFloat(expenseAmount.value), 
-        id: counter
-    }
-    expenseList.push(expenseDetails);
-
-
-    populateList(expenseList);
-
-    
-
-    let options = document.createElement('li');
-    let appendOptions =  editAndDelete.appendChild(options);
-
-    
-    let deleteItem = document.createElement('a');
-    deleteItem.href = '#';
-    deleteItem.innerText = 'delete';
-    deleteItem.addEventListener('click', () => { 
-        deleteItemFunc(expenseList) 
-    });
-    appendOptions.appendChild(deleteItem);
-
-    let deleteItemFunc = (arr) => {
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i].id === expenseDetails.id){
-                arr.splice(i, 1)
-                console.log('splice init', arr);
-                expenseTitle.removeChild(expenseTitle.children[i]);
-                expenseValue.removeChild(expenseValue.children[i]);
-                editAndDelete.removeChild(editAndDelete.children[i]);
-            }
-        }    
-    }
-   
-    calculateBalanceTotal();
-    calculateExpenseTotal();
-})
-
-// calculates the balance total 
+// calculates the balance total
 let calculateBalanceTotal = () => {
-    let total = 0;
-    let numBalanceTotal =  parseFloat(balanceTotal.innerText);
-    let numExpenseAmount = parseFloat(expenseAmount.value)
+  balanceTotal.innerText = 
+    parseFloat(budgetTotal.innerText) - parseFloat(expenseTotal.innerText);
+};
 
-    total = numBalanceTotal - numExpenseAmount;
+// calculates the expense total
+let calculateExpenseTotal = () => {
+    let expenseSum  = 0 ;
+    expenseList.forEach(expense => {
+        expenseSum += expense.value
+    })
+    return expenseTotal.innerText = expenseSum;
 
-    return balanceTotal.innerHTML = total;
-}
+};
 
-// calculates the expense total 
-let calculateExpenseTotal = ()=> {
-    let total = 0;
-    let numExpenseTotal =  parseFloat(expenseTotal.innerText);
-    let numExpenseAmount = parseFloat(expenseAmount.value)
+// let editItem = document.createElement('a');
+// editItem.href = '#';
+// editItem.innerText = ' edit';
+// editItem.addEventListener('click', () => {
+//     editItemFunc(expenseDetails)
+// });
+// let appendEdit = appendOptions.appendChild(editItem);
 
-    total = numExpenseTotal + numExpenseAmount;
-
-    return expenseTotal.innerText = total;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let editItem = document.createElement('a');
-    // editItem.href = '#';
-    // editItem.innerText = ' edit';
-    // editItem.addEventListener('click', () => { 
-    //     editItemFunc(expenseDetails) 
-    // });
-    // let appendEdit = appendOptions.appendChild(editItem);
-
-    // let editItemFunc = (expdet) => {
-    //     for(let i = 0; i < expenseList.length; i++){
-    //         if(expenseList[i].id == expdet.id){
-    //             expenseList.splice(i, 1,  )
-    //             console.log('edit init', expenseList);    
-    //         }
-    //     }
-    // }
+// let editItemFunc = (expdet) => {
+//     for(let i = 0; i < expenseList.length; i++){
+//         if(expenseList[i].id == expdet.id){
+//             expenseList.splice(i, 1,  )
+//             console.log('edit init', expenseList);
+//         }
+//     }
+// }
